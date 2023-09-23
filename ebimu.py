5b10d5a28b088646f9e3410a8cad1b9657e3f5fe
taeseokyang <ts.yang.0123@gmail.com>
2023-09-23 16:58:11 +0100

    first

---
+++ ebimu.py
@@ -0,0 +1,20 @@
+import time
+import serial
+
+#parsing = " "
+
+#ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.001)#usb
+ser = serial.Serial('/dev/ttyAMA1',115200,timeout=0.001)#tx4,rx5
+buf = ""
+while True:
+    while ser.inWaiting():
+        data = str(ser.read()).strip()
+        buf += data
+        #print(data)
+        if data[3] == "n":
+            buf = buf.replace("'","")
+            buf = buf.replace("b","")
+            roll, pitch, yaw = map(float,buf[2:-4].split(','))
+            print(roll,pitch,yaw)
+            buf = " "
+    time.sleep(0.05)

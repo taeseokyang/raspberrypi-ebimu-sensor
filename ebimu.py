4a4bcffdc712440ee41402015926d24e8b830c9e
taeseokyang <ts.yang.0123@gmail.com>
2023-09-24 02:13:45 +0100

    code

--- ebimu.py
+++ ebimu.py
@@ -1,5 +1,33 @@
 import time
 import serial
+from matplotlib import pyplot as plt
+import numpy as np
+
+max_points = 100
+fig = plt.figure()
+plt.subplots_adjust(left=0.125, bottom-0.1, right=0.9, top=0.9, wspace=0.2, hspace=0.5)
+accelAxes = fig.add_subplot(1,1,1,xlim(0, max_points), ylim=(-3.0,3.0))
+
+accelAxes.set_title("test")
+accelLineX, = accelAxes.plot([],[], lw=2)
+accelLineX, = accelAxes.plot(np.arange(max_points), np.ones(max_points, dtype=np.float64)*np.nan, lw=2, color='red', label='x')
+
+accelXBunch = []
+
+
+
+
+def init():
+    return accelLineX
+
+def aniate(i):
+    
+
+def serialSensorRead():
+    while ser.inWating():
+        data = str(ser.read()).strip()
+        buf += data
+        
 
 #ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.001)#usb
 ser = serial.Serial('/dev/ttyAMA1',115200,timeout=0.001)#tx4,rx5
@@ -12,6 +40,8 @@ while True:
             buf = buf.replace("'","")
             buf = buf.replace("b","")
             roll, pitch, yaw = map(float,buf[2:-4].split(','))
+            accelXBunch.append(roll)
             print(roll,pitch,yaw)
             buf = " "
     time.sleep(0.05)
+    plt.show()

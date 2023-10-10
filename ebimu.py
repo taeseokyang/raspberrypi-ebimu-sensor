653f2fea9bd076810ccf7d2d3053b171f8bee2bb
taeseok yang <136783693+taeseokyang@users.noreply.github.com>
2023-10-11 00:50:35 +0100

    Update ebimu.py

--- ebimu.py
+++ ebimu.py
@@ -6,13 +6,14 @@ fileName = nowTime[:10]+"_"+nowTime[11:21]
 
 try:
     f = open(fileName, 'w')
+    log = open("log.txt",'w')
 except:
     print("Failed to open file")
 
-#ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.001) #when connect to usb
-ser = serial.Serial('/dev/ttyAMA1',115200,timeout=0.001) #when connect to pins (tx4,rx5)
-buf = ""
+ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.001) #when connect to usb
+#ser = serial.Serial('/dev/ttyAMA1',115200,timeout=0.001) #when connect to pins (tx4,rx5)
 
+buf = ""
 while True:
     # read value
     while ser.inWaiting():
@@ -23,17 +24,22 @@ while True:
             buf = buf.replace("b","") 
             
             # split each data
-            try :roll, pitch, yaw = map(float,buf[1:-4].split(','))
-            except:
-                print("Invalid Data")
+            try :
+                roll, pitch, yaw, x, y, z = map(float,buf[1:-4].split(','))
+            except Exception as e:
+                print("Error from data processing : ", e)
+                log.write(str(e)+"\n")
+                buf = ""
                 continue
             
-            try :f.write(data)
-            except :
-                print("File write failed")
+            try :
+                datas = [roll,pitch,yaw,x,y,z]
+                writeString = "*"+str(datas)[1:-1]+"\n"
+                f.write(writeString)
+            except Exception as e:
+                print("Error from file writing : ", e)
+                log.write(str(e)+"\n")
                 continue
 
-            print(roll,pitch,yaw)
+            print(roll,pitch,yaw,x,y,z)
             buf = ""
-
-# f.close()
